82fc1c0cd3b912c7f365c52432a70d3c82cb075a
SNarboni <sebastien.narboni@hotmail.fr>
2021-02-15 17:27:34 +0100

    ajout de react router dom

--- trippy/src/App.js
+++ trippy/src/App.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
-import NavBar from './components/NavBar';
+import React from "react";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import Home from "./views/Home";
+import Hotels from "./views/Hotels";
+import HotelsPages from "./views/HotelsPages";
+import Error from "./views/Error";
+import NavBar from "./components/NavBar";
+import "./App.css";
 
-class App extends React.Component{
-  render(){
-    return (
-        <NavBar/>
-      );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
   }
-  
+
+  render() {
+    return (
+      <BrowserRouter>
+        <NavBar></NavBar>
+
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route path="/hotels/:city" component={Hotels}></Route>
+          <Route path="/hotels/:id" component={HotelsPages}></Route>
+          <Route path="*" component={Error}></Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- trippy/src/components/HotelCard.jsx
+++ trippy/src/components/HotelCard.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
-import './App.css';
+import '../App.css';
 
 class HotelCard extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      
+    }
+  }
   render(){
     return (
         <h1>HotelCard</h1>
